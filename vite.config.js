00317bfa2c64ce6a10bd82980e03d2590057e390
Naidelyn Maldonado <72952823+NaiDevs@users.noreply.github.com>
2023-11-04 11:32:15 -0600

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,36 @@
+import { defineConfig } from 'vite'
+import * as glob from 'glob';
+import path, { resolve } from 'node:path';
+import { ViteMinifyPlugin} from 'vite-plugin-minify'
+import htmlPurge from 'vite-plugin-purgecss';
+import handlebars from 'vite-plugin-handlebars';
+
+import handlerBarsContext from './variables.js';
+
+export default defineConfig({
+    base: "/portafolio-actualizado/",
+    appType: 'mpa',
+    build: {
+        rollupOptions: {
+            input: Object.fromEntries(
+                [...glob.sync('./!(dist)/*.html').map(file => [
+                    file.slice(0, file.length - path.extname(file).length), resolve(__dirname, file)
+                ]),
+                ...glob.sync('./*.html').map(file => [
+                    file.slice(0, file.length - path.extname(file).length), resolve(__dirname, file)
+                ])]
+            ),
+        },
+    },
+    plugins: [
+        handlebars({
+            partialDirectory: resolve(__dirname, 'partials'),
+            context: (pagePath) => {
+                return handlerBarsContext[pagePath];
+            },
+        }),
+        htmlPurge({}),
+        ViteMinifyPlugin({}),
+    ],
+    base: "/portafolio-naidelyn/"
+})
