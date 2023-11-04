ef7ed554e538aba88173a3c4f2ac9d7110d627f4
Naidelyn Maldonado <naidelynmaldonado54@gmail.com>
2023-11-04 10:55:52 -0600

    change

---
+++ variables.js
@@ -0,0 +1,13 @@
+const handlerBarsContext = {
+    "/index.html": {
+        "title": "Inicio"
+    },
+    "/contacts/contact.html": {
+        "title": "Contacto"
+    },
+    "/servicios.html": {
+        "title": "Servicios"
+    }
+}
+
+export default handlerBarsContext;
